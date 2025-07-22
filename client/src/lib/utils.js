4516392463a7588e1dfdb0341767cfed7686adae
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-07-22 13:47:22 +0300

    Created Donation flow

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,6 +1,5 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+export function formatDate(dateString) {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const date = new Date(dateString);
+  return date.toLocaleDateString(undefined, options);
 }

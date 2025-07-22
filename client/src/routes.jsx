e80ff12cbb9c0fdbda7454377ed6015950a62403
Brian <okellobrian18@gmail.com>
2025-07-22 13:05:17 +0300

    Token Validation after account creation. Cleaning up unnecessary files.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SignUp from './components/Auth/SignUp/SignUp';
 import SignIn from './components/Auth/SignIn/SignIn';
 import { Route, Routes } from 'react-router-dom';
+import TokenValidation from './components/Auth/TokenValidation/TokenValidation';
 
 
 const routes = () => {
@@ -9,6 +10,7 @@ const routes = () => {
     <Routes>
       <Route path="/Sign-In" element={<SignIn />} />
       <Route path="/Sign-Up" element={<SignUp />} />
+      <Route path="/Token" element={<TokenValidation />} />
     </Routes>
   );
 }

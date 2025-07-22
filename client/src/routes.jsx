27c4a11f5b96c84cb9c33671cb558a68138e7fa4
Cornelius Chebet <cornelius.chebet@student.moringaschool.com>
2025-07-22 21:49:07 +0300

    updated routes.jsx

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React from 'react';
 import SignUp from './components/Auth/SignUp/SignUp';
 import SignIn from './components/Auth/SignIn/SignIn';
 import { Route, Routes } from 'react-router-dom';
-import TokenValidation from './components/Auth/TokenValidation/TokenValidation';
 
-
-const routes = () => {
+const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<SignIn />} />
       <Route path="/Sign-In" element={<SignIn />} />
       <Route path="/Sign-Up" element={<SignUp />} />
-      <Route path="/Token" element={<TokenValidation />} />
     </Routes>
   );
 }
 
-export default routes
+export default AppRoutes;

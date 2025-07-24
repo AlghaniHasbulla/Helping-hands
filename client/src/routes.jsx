0e9ffd784c6698647264847d93f05f9876396224
Brian <okellobrian18@gmail.com>
2025-07-24 21:34:47 +0300

    MInor route adjustment

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
 import SignUp from './components/Auth/SignUp/SignUp';
 import SignIn from './components/Auth/SignIn/SignIn';
-import { Route, Routes } from 'react-router-dom';
+import DonationRequestForm from './components/Donations/DonationRequestForm';
+import NGORequestsHistory from './components/Donations/NGORequestsHistory';
+import DonorHome from './components/Donations/DonorHome';
+import DonationForm from './components/Donations/DonationForm';
+import DonationHistory from './components/Donations/DonationHistory';
+import Home from './components/Home/Home';
+import Causes from './components/Causes/Causes';
+import Events from './components/Events/Events';
+import About from './components/About/About';
+import Contacts from './components/Contacts/Contacts';
 
-const AppRoutes = () => {
+
+
+const routes = () => {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/causes" element={<Causes />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contacts" element={<Contacts />} />
       <Route path="/Sign-In" element={<SignIn />} />
       <Route path="/Sign-Up" element={<SignUp />} />
       <Route path="/donation-request" element={<DonationRequestForm />} />
@@ -18,4 +34,4 @@ const AppRoutes = () => {
   );
 }
 
-export default AppRoutes;
+export default routes

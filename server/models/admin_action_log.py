567b38f5b3c68439e66f0d8e25448f11f2fe933c
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-07-25 13:38:22 +0300

    adding admilog model and relationship in user

---
+++ server/models/admin_action_log.py
@@ -0,0 +1,14 @@
+from server.extensions import db
+from datetime import datetime
+
+class AdminActionLog(db.Model):
+    __tablename__ = 'admin_action_logs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    actor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    action = db.Column(db.String(255), nullable=False)
+    target_user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    actor = db.relationship('User', foreign_keys=[actor_id], back_populates='performed_actions')
+    target = db.relationship('User', foreign_keys=[target_user_id], back_populates='targeted_by_actions')
--- server/models/user.py
+++ server/models/user.py
@@ -4,11 +4,9 @@ from flask_bcrypt import Bcrypt
 from server.extensions import db
 from sqlalchemy_serializer import SerializerMixin
 
-
 bcrypt = Bcrypt()
 
-class User(db.Model,SerializerMixin):
-
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -19,10 +17,26 @@ class User(db.Model,SerializerMixin):
     is_verified = db.Column(db.Boolean, default=False)
     avatar_url = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    verification_token =db.Column(db.String,nullable=True)
+    verification_token = db.Column(db.String, nullable=True)
     verification_token_expiry = db.Column(db.DateTime, nullable=True)
+
     donation_requests = db.relationship('DonationRequest', back_populates='ngo', lazy=True)
   
+    # Admin activity relationships
+    performed_actions = db.relationship(
+        'AdminActionLog',
+        foreign_keys='AdminActionLog.actor_id',
+        back_populates='actor',
+        lazy=True
+    )
+
+    targeted_by_actions = db.relationship(
+        'AdminActionLog',
+        foreign_keys='AdminActionLog.target_user_id',
+        back_populates='target',
+        lazy=True
+    )
+
     serialize_rules = ('-donation_requests.ngo',)
 
     def set_password(self, password):

5eff4b3670449c9e18d676e9fab505c59f46ad2a
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-07-28 09:58:04 +0300

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes";
 import Navbar from './components/Navigation/Navbar';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <AppRoutes />
+      <ErrorBoundary>
+        <AppRoutes />
+      </ErrorBoundary>
     </Router>
   );
 }

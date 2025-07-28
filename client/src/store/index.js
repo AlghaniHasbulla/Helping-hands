3adb48deddf207c37e6324264c3386994e9e9e07
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-07-28 09:56:46 +0300

    updated index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import donationsReducer from './donationsSlice';
+import authReducer from './authSlice';
 
 const store = configureStore({
   reducer: {
     donations: donationsReducer,
+    auth: authReducer,
   },
 });
 

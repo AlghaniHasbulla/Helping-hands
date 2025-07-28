2a1931f0538ec5e791cb24158c8305d64cac0e94
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-07-28 09:49:50 +0300

    updated api.js

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import store from '../store';
 
 const api = axios.create({
   baseURL: 'https://helping-hands-backend-w4pu.onrender.com/',
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const state = store.getState();
+    const token = state.auth?.accessToken || localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;

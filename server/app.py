f80be995bec62e86a9fd6d3fe2f162879b3e3710
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-07-31 09:48:33 +0300

    Adding frontend  link on cors

--- server/app.py
+++ server/app.py
@@ -20,7 +20,7 @@ def create_app(testing=False):
          origins=[
              "http://localhost:5173",
              "http://localhost:3000",
-             "https://your-frontend-domain.com"  # Add your production domain here
+             "https://helpinghands-umber.vercel.app/" 
          ],
          supports_credentials=True,
          methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"], 
